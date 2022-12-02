9c1738cfa31685c6bb5d237457b9bd670236504a
Dan <frivolousvision@gmail.com>
2022-12-02 12:40:12 -0500

    updated strict transport security

--- API/Startup.cs
+++ API/Startup.cs
@@ -82,7 +82,7 @@ namespace API
             {
                 app.Use(async (context, next) =>
                 {
-                    context.Response.Headers.Add("Strict-Transport-Secuirty", "max-age=31536000");
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
                     await next.Invoke();
                 });
             }
